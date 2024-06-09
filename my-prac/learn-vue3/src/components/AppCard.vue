7744010d7732578f96ab448eb4b498ea41ce0d9d
cpg7023 <cpg7023@nate.com>
2024-06-10 00:26:34 +0900

    from home

--- my-prac/learn-vue3/src/components/AppCard.vue
+++ my-prac/learn-vue3/src/components/AppCard.vue
@@ -1,80 +1,29 @@
 <template>
 	<div class="card">
-		<div class="card-body">
-			<!-- type: news, notice-->
-			<span class="badge text-bg-secondary">{{ typeName }}</span>
-			<h5 class="card-title red mt-2">{{ title }}</h5>
-			<p class="card-text">{{ contents }}</p>
-			<a href="#" class="btn" :class="isLikeClass">좋아요</a>
+		<div v-if="$slots.header" class="card-header">
+			<slot name="header" headerMessage="헤더 메시지">#Header</slot>
 		</div>
-		<button @click="toggleLike">toggle</button>
+		<!-- {{ $slots }} -->
+		<div v-if="$slots.default" class="card-body">
+			<slot childMessage="childMessage" helloMessage="하이룽">#Body</slot>
+		</div>
+		<div v-if="hasFooter" class="card-footer text-body-secondary">
+			<slot name="footer" footerMessage="푸터 메시지">#Footer</slot>
+		</div>
 	</div>
+	<br />
 </template>
 
 <script>
-import { onMounted } from 'vue';
-import { computed, ref, useCssModule } from 'vue';
-
+import { ref, computed } from 'vue';
 export default {
-	emits: ['toggleLike'],
-	props: {
-		type: {
-			type: String,
-			defualt: 'news',
-			validator: value => {
-				return ['news', 'notice'].includes(value);
-			}
-		},
-		title: {
-			type: String,
-			required: true
-		},
-		contents: {
-			type: String
-			//required: true
-		},
-		isLike: {
-			type: Boolean,
-			default: false
-		},
-		obj: {
-			type: Object,
-			default: () => {}
-		}
-	},
-	setup(props, context) {
-		//console.log('props.title:', props.title);
-		const isLikeClass = computed(() =>
-			props.isLike ? 'btn-danger' : 'btn-outline-danger'
-		);
+	setup({ slots }) {
+		const childMessage = ref('자식 메시지 임');
 
-		const typeName = computed(() =>
-			props.type === 'news' ? '뉴스' : '공지사항'
-		);
-
-		const toggleLike = () => {
-			context.emit('toggleLike');
-		};
-		return { isLikeClass, typeName, toggleLike };
+		const hasFooter = computed(() => !!slots.footer);
+		return { childMessage, hasFooter };
 	}
-	// onMounted(props) {
-	// 	const isListClass = computed(() =>
-	// 		props.isLike ? 'btn-danger' : 'btn-outline-danger'
-	// 	);
-	// 	return { isListClass };
-	// }
 };
 </script>
 
-<style></style>
-
-<!-- <style module>
-.red {
-	color: v-bind(color) !important;
-}
-</style> -->
-<!-- <style scoped>
-.red {
-	color: red !important;
-}
-</style> -->
+<style lang="scss" scoped></style>
